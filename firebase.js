d74e17a96dca47a8607e3663822793b2b2bc9388
Cabinet-Mohammedi <cabinet.mohammedi17@gmail.com>
2025-11-20 15:44:59 +0100

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,14 @@
-{
-  "rules": {
-    "rendezvous": {
-      ".read": "auth != null",  // قراءة المواعيد فقط للطبيب المسجل
-      ".write": true             // أي شخص يمكنه حجز موعد
-    },
-    "medecin": {
-      ".read": "auth != null",  // فقط الطبيب يمكنه قراءة البيانات
-      ".write": "auth != null"  // فقط الطبيب يمكنه تعديل البريد/كلمة المرور
-    }
-  }
-}
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDaituJimoLNpkMYvr1u4KJC8XEJgbrGZA",
+  authDomain: "clinique-9f351.firebaseapp.com",
+  databaseURL: "https://clinique-9f351-default-rtdb.firebaseio.com",
+  projectId: "clinique-9f351",
+  storageBucket: "clinique-9f351.appspot.com",
+  messagingSenderId: "537502134144",
+  appId: "1:537502134144:web:a7d0ba7fb48f97b2775b65"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
