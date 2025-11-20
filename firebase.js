feda2b58081e2c7be80745800b32a85a492d683b
Cabinet-Mohammedi <cabinet.mohammedi17@gmail.com>
2025-11-20 16:13:47 +0100

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,19 @@
-// firebase.js
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-import { getAuth } from "firebase/auth";
+<script src="https://www.gstatic.com/firebasejs/10.12.3/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.12.3/firebase-database-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.12.3/firebase-auth-compat.js"></script>
+<script>
+  const firebaseConfig = {
+    apiKey: "AIzaSyDaituJimoLNpkMYvr1u4KJC8XEJgbrGZA",
+    authDomain: "clinique-9f351.firebaseapp.com",
+    databaseURL: "https://clinique-9f351-default-rtdb.firebaseio.com",
+    projectId: "clinique-9f351",
+    storageBucket: "clinique-9f351.appspot.com",
+    messagingSenderId: "537502134144",
+    appId: "1:537502134144:web:a7d0ba7fb48f97b2775b65"
+  };
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDaituJimoLNpkMYvr1u4KJC8XEJgbrGZA",
-  authDomain: "clinique-9f351.firebaseapp.com",
-  databaseURL: "https://clinique-9f351-default-rtdb.firebaseio.com",
-  projectId: "clinique-9f351",
-  storageBucket: "clinique-9f351.appspot.com",
-  messagingSenderId: "537502134144",
-  appId: "1:537502134144:web:a7d0ba7fb48f97b2775b65"
-};
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.database();
+  const auth = firebase.auth();
+</script>
 
-export const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
-export const auth = getAuth(app);
